0c316d4ea39f2f595a63840173b4bcdc23194cbd
Thiago Resende <thiago.gcresende@gmail.com>
2019-02-08 22:01:19 -0200

    Initial Commit

---
+++ alura-viagens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alura-viagens
+//
+//  Created by Thiago Resende on 2/8/19.
+//  Copyright © 2019 Thiago Resende. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
